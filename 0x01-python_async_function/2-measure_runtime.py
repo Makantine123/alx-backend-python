837159b69abcb284a722c1547495cbf8f37523e5
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-10-09 21:03:22 +0200

    Commit

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Module contains measure_time function
+"""
+
+from asyncio import run
+from time import time
+
+wait_n = __import__("1-concurrent_coroutines").wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    """
+    @n: Times to spawn wait_random
+    @max_delay: Input for wait_random
+    returns: float
+    """
+    start = time()
+    run(wait_n(n, max_delay))
+    end = time()
+    total_time = end - start
+    return total_time / n
