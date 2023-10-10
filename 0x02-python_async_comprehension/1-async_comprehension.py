353f543a8acdb010899461e8ae7fea523465da45
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-10-10 08:40:26 +0200

    Commit

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Module contains coroutine async_comprehension
+"""
+
+import asyncio
+
+async_generator = __import__("0-async_generator").async_generator
+
+
+async def async_comprehension():
+    """
+    Async comprehensions
+    """
+    return [i async for i in async_generator()]
